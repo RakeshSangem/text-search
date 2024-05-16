151111ad405daa536df1f415cb93edeeb8fad8b0
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-05-16 15:21:31 +0530

    chore: UI improvement

--- src/components/FileViewer.tsx
+++ src/components/FileViewer.tsx
@@ -25,7 +25,7 @@ export default function FileViewer({ file, onClose }: FileViewerProps) {
 
     const newContent = file.content.replace(
       regex,
-      `<mark class="bg-yellow-200">${searchValue}</mark>`
+      `<mark class="bg-yellow-300 rounded-md">${searchValue}</mark>`
     );
 
     setNewContent(newContent);
@@ -36,10 +36,10 @@ export default function FileViewer({ file, onClose }: FileViewerProps) {
       <div className="flex items-center justify-between rounded-t-md bg-[#F5F5F5] p-1 px-4 text-gray-700">
         <button
           onClick={() => onClose && onClose(null)}
-          className="grid h-4 w-4 place-items-center rounded-full bg-[#FF5F57]"
+          className="group grid h-4 w-4 place-items-center rounded-full bg-[#FF5F57] hover:bg-[#FF3F3F] focus:outline-none focus:ring-2 focus:ring-red-500/50 transition-colors"
         >
           <svg
-            className="text-black/45"
+            className="text-black/45 hidden group-hover:block group-focus:block"
             width="1rem"
             height="1rem"
             viewBox="0 0 24 24"
@@ -53,13 +53,21 @@ export default function FileViewer({ file, onClose }: FileViewerProps) {
           </svg>
         </button>
         <h2 className="">{file.name}</h2>
-        <div className="">
-          <Input
-            type="search"
-            onChange={onSearch}
-            className="my-1 min-w-16 rounded-md border border-[#D1D1D1] bg-transparent px-2 py-1 ring-blue-500/50 focus:outline-none focus:ring-2"
-            placeholder="Search... "
-          />
+        <div className="flex items-center space-x-1 relative">
+          <div className="relative">
+            <Input
+              type="search"
+              onChange={onSearch}
+              className="reltive min-w-16 rounded-md border border-[#D3D3D3] bg-transparent px-2 py-0.5 text-sm ring-blue-500/20 focus:outline-none focus:ring-2"
+              placeholder="Search... "
+            />
+            <div className="absolute text-xs p-0.5 rounded-md right-0.5 top-0.5 text-slate-400 border border-gray-300 bg-[#F5F5F5]">
+              ⌘K
+            </div>
+          </div>
+          <button className="active:from-transparent-black rounded bg-[#007AFF] bg-gradient-to-b from-white/10 to-transparent px-2 py-0.5 text-sm text-white shadow-md">
+            Search
+          </button>
         </div>
       </div>
       {/* Text content */}
@@ -69,17 +77,20 @@ export default function FileViewer({ file, onClose }: FileViewerProps) {
         }}
         className="flex-grow overflow-y-scroll rounded-b-md bg-white/95 p-2 px-4 text-black/90"
       />
-      <div className="flex w-full bg-[#F5F5F5] px-4 py-1 text-zinc-600 gap-x-4">
+      <div className="flex w-full bg-[#F5F5F5] px-4 py-1 text-zinc-600 gap-x-4 rounded-b-md border-gray-200/50 border-t">
         <p className="text-sm">
           Word count:
           <span className="text-sm font-medium text-zinc-800 ml-1">
             {countWords(file.content)}{" "}
           </span>
         </p>
-        <p className="text-sm">
+        <p className="text-sm flex items-center">
           Total Matches:
-          <span className="text-sm font-medium text-zinc-800 ml-1">
+          <span className="text-sm font-medium text-zinc-800 ml-1 block w-6">
             {occurrences}
+          </span>
+          <span className="font-light text-zinc-600 text-xs">
+            (Occurrences)
           </span>
         </p>
       </div>
